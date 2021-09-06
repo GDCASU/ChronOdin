74f6789dd925399a1553f2dac6638451c216a0df
Voidijure <54921557+CristionD@users.noreply.github.com>
2021-09-06 12:25:40 -0700

    Create pickup script and alter Player for camera smoothness
    ObjectPickup script is complete, documented, and attached to Player. The Player's rigidbody has interpolation, and the Player Camera has a rigidbody with interpolation as well to avoid stuttering when Player is moving an object around.

--- Assets/Imports/Scripts/ObjectPickup.cs
+++ Assets/Imports/Scripts/ObjectPickup.cs
@@ -1,126 +1,105 @@
+/*
+ * Allows the Player to pick up liftable objects upon pressing [Tab].
+ * Objects are released when Player ceases pressing [Tab].
+ * 
+ * Author: Cristion Dominguez
+ * Date: 6 Sept. 2021
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectPickup : MonoBehaviour
 {
+    [Tooltip("The camera attached to Player")]
     [SerializeField]
     Transform playerCamera;
 
     [SerializeField]
-    Transform pickupDestination;
-
-    [SerializeField]
-    [Tooltip("")]
+    [Tooltip("The maximum distance the Player can pick up an object")]
     private float maxPickupDistance = 10f;
 
     [SerializeField]
-    [Tooltip("")]
-    private float returnSpeed = 0.1f;
+    [Tooltip("The amount of force that can be applied to the held object before Player releases it")]
+    private float releaseForce;
 
-    private Transform heldObject = null;
-    private Rigidbody objectPhysics = null;
-    private Collider objectCollider = null;
-    private RaycastHit rayHit;
-
+    private Transform heldObject = null;  // the transform of the picked up object
+    private Rigidbody objectPhysics = null;  // the rigidbody of the picked up object
+    private RaycastHit rayHit;  // info about ray collisions
+    
+    // The previous interpolation and detection mode of the picked up object.
     private RigidbodyInterpolation previousInterpolation = RigidbodyInterpolation.None;
     private CollisionDetectionMode previousDetectionMode = CollisionDetectionMode.Discrete;
-    private Vector3 previousPosition = Vector3.zero;
-    float timeSinceLastSave = 0f;
 
+    FixedJoint objectJoint;  // the joint instantiated between playerCamera and heldObject
+
+    /// <summary>
+    /// Attempts to pick up an object when Player presses down [Tab]. 
+    /// Releases object when its joint is broken or the Player ceases pressing down [Tab].
+    /// </summary>
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             Pickup();
         }
-        else if(Input.GetKeyUp(KeyCode.Tab))
-        {
-            Release();
-        }
 
-        /*
         if (heldObject != null)
         {
-            if (Physics.Raycast(previousPosition, heldObject.position - previousPosition, out rayHit, Vector3.Distance(previousPosition, heldObject.position)))
-            {
-                if (!rayHit.collider.Equals(objectCollider))
-                {
-                    heldObject.LookAt(rayHit.normal, Vector3.up);
-                    heldObject.position = rayHit.point + rayHit.normal * heldObject.lossyScale.y / 2;
-
-                    previousPosition = heldObject.position;
-                    timeSinceLastSave = 0;
-                }
-            }
-
-            if (timeSinceLastSave >= 0.5f)
-            {
-                previousPosition = heldObject.position;
-                timeSinceLastSave = 0;
-            }
-
-            timeSinceLastSave += Time.deltaTime;
-        }
-        */
-    }
-
-    private void FixedUpdate()
-    {
-        if(heldObject != null)
-        {
-            //heldObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            //heldObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-
-            if (heldObject.position != pickupDestination.position)
-            {
-                //heldObject.position = Vector3.MoveTowards(heldObject.position, pickupDestination.position, returnSpeed);
-
-                Vector3 dir = pickupDestination.position - heldObject.position;
-                //objectPhysics.velocity = dir.normalized * returnSpeed;
-
-                objectPhysics.MovePosition(pickupDestination.position);
-            }
-
-            if (Vector3.Distance(heldObject.position, playerCamera.position) > maxPickupDistance)
+            if ((objectJoint == null) || Input.GetKeyUp(KeyCode.Tab))
             {
                 Release();
             }
         }
     }
 
+    /// <summary>
+    /// Casts a ray from playerCamera in the forward direction. If the ray hits an object with the "Liftable" tag, the transform
+    /// and rigidbody of the object is captured. Then the object's gravity is deactivated, and its interpolation and detection modes
+    /// are altered. Then a fixed joint between the playerCamera and object is instantiated with a specified break force.
+    /// </summary>
     private void Pickup()
     {
-        if(Physics.Raycast(playerCamera.position, playerCamera.TransformDirection(Vector3.forward), out rayHit, maxPickupDistance, 1<<6))
+        if(Physics.Raycast(playerCamera.position, playerCamera.TransformDirection(Vector3.forward), out rayHit, maxPickupDistance))
         {
-            heldObject = rayHit.transform;
-            objectPhysics = heldObject.GetComponent<Rigidbody>();
-            objectCollider = heldObject.GetComponent<Collider>();
+            if (rayHit.transform.tag.Equals("Liftable"))
+            {
+                // Capture the object's transform and rigidbody.
+                heldObject = rayHit.transform;
+                objectPhysics = heldObject.GetComponent<Rigidbody>();
 
-            objectPhysics.useGravity = false;
-            previousInterpolation = objectPhysics.interpolation;
-            previousDetectionMode = objectPhysics.collisionDetectionMode;
-            objectPhysics.interpolation = RigidbodyInterpolation.Interpolate;
-            objectPhysics.collisionDetectionMode = CollisionDetectionMode.Continuous;
+                // Alter the object's physics properties.
+                objectPhysics.useGravity = false;
+                previousInterpolation = objectPhysics.interpolation;
+                previousDetectionMode = objectPhysics.collisionDetectionMode;
+                objectPhysics.interpolation = RigidbodyInterpolation.Interpolate;
+                objectPhysics.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
 
-            //heldObject.parent = playerCamera;
-            pickupDestination.position = heldObject.position;
-
-            previousPosition = heldObject.position;
-            timeSinceLastSave = 0;
+                // Add joint between held object and Player camera, and then assign a break force.
+                objectJoint = heldObject.gameObject.AddComponent<FixedJoint>();
+                objectJoint.connectedBody = playerCamera.GetComponent<Rigidbody>();
+                objectJoint.breakForce = releaseForce;
+            }
         }
     }
 
+    /// <summary>
+    /// Releases the held object from Player's grasp, which restores the object's properties before being
+    /// picked up and destroys the joint if it did not break.
+    /// </summary>
     private void Release()
     {
-        if(heldObject != null)
-        {
-            objectPhysics.useGravity = true;
-            objectPhysics.interpolation = previousInterpolation;
-            objectPhysics.collisionDetectionMode = previousDetectionMode;
+        // Restore object properties before pickup.
+        objectPhysics.useGravity = true;
+        objectPhysics.interpolation = previousInterpolation;
+        objectPhysics.collisionDetectionMode = previousDetectionMode;
 
-            heldObject.parent = null;
-            heldObject = null;
-        }
+        // Destroy joint if it did not break.
+        if (objectJoint != null)
+            Destroy(objectJoint);
+
+        // Assign the held object as nothing.
+        heldObject = null;
     }
 }
