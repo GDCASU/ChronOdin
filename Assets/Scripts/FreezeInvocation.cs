0752fac13d33918f2efc02eea3e39036f2339244
Voidijure <54921557+CristionD@users.noreply.github.com>
2021-09-13 18:28:47 -0700

    Separate the ObjectFreeze script into 2 scripts
    The ObjectFreeze script was separated into 2 scripts. ObjectFreeze now freezes objects for a specified amount of times. FreezeInvocation invokes objects to freeze and enforces the cooldown.

---
+++ Assets/Scripts/FreezeInvocation.cs
@@ -0,0 +1,138 @@
+/*
+ * Freezes the attached gameobject for a specified amount of time; cooldown activates right when the object freezes. After the object is unfrozen, it can not be frozen whilst the cooldown is active.
+ * 
+ * Author: Cristion Dominguez
+ * Date: 10 September 2021
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FreezeInvocation : MonoBehaviour
+{
+    [Header("Button")]
+    [Tooltip("The button to freeze a single object")]
+    [SerializeField]
+    private KeyCode freezeSingleButton = KeyCode.F;
+
+    [Tooltip("The button to freeze the entire environment")]
+    private KeyCode freezeEnvironmentButton = KeyCode.G;
+
+    [Header("Time Values")]
+    [Tooltip("The time the object shall be frozen for")]
+    [SerializeField]
+    private float freezeSingleTime = 5f;
+
+    [Tooltip("The duration the object can't be frozen after being frozen")]
+    [SerializeField]
+    private float freezeSingleCooldown = 5f;
+
+    [Tooltip("The duration the environment shall be frozen for")]
+    [SerializeField]
+    private float freezeEnvironmentTime = 10f;
+
+    [Tooltip("The duration the environment can't be frozen after being frozen")]
+    [SerializeField]
+    private float freezeEnvironmentCooldown = 10f;
+
+    [Header("Transforms")]
+    [Tooltip("The camera providing the Player vision")]
+    [SerializeField]
+    private Transform playerCamera;
+
+    // For suspending coroutines.
+    private WaitForSeconds waitForSingleTime;
+    private WaitForSeconds waitForSingleCooldown;
+    private WaitForSeconds waitForEnvironmentTime;
+    private WaitForSeconds waitForEnvironmentCooldown;
+
+    private bool canInitiateSingleFreeze = true;  // Is the object unfrozen and is the cooldown inactive?
+    private bool canInitiateEnvironmentFreeze = true;
+
+    ObjectFreeze objectToFreeze = null;
+
+    public static Func<float, IEnumerator> freezeEveryObject;
+
+    /// <summary>
+    /// Gathers the rigidbody of the gameobject and assigns coroutine suspension times.
+    /// </summary>
+    private void Start()
+    {
+        waitForSingleTime = new WaitForSeconds(freezeSingleTime);
+        waitForSingleCooldown = new WaitForSeconds(freezeSingleCooldown);
+
+        waitForEnvironmentTime = new WaitForSeconds(freezeEnvironmentTime);
+        waitForEnvironmentCooldown = new WaitForSeconds(freezeEnvironmentCooldown);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Update()
+    {
+        if (Input.GetKeyDown(freezeSingleButton) && canInitiateSingleFreeze)
+        {
+            // Set the ray's starting position and direction.
+            Vector3 startRayPosition = playerCamera.position;
+            Vector3 rayDirection = playerCamera.TransformDirection(Vector3.forward);
+            int maxRayCasts = 2;
+            RaycastHit rayHit;
+
+            // Cast the ray until the ray does not hit the Player or maxRayCasts has been reached.
+            for (int i = 0; i < maxRayCasts; i++)
+            {
+                if (Physics.Raycast(startRayPosition, rayDirection, out rayHit))
+                {
+                    objectToFreeze = rayHit.transform.GetComponent<ObjectFreeze>();
+
+                    if (objectToFreeze != null)
+                    {
+                        canInitiateSingleFreeze = false;
+                        StartCoroutine(objectToFreeze.FreezeObject(freezeSingleTime));
+                        StartCoroutine(ActivateSingleCooldown());
+                    }
+                    // If the ray hits nothing, stop casting rays.
+                    // FOR TESTING PURPOSES, comment this "else" block out.
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+            
+        }
+
+        if (Input.GetKeyDown(freezeEnvironmentButton) && canInitiateEnvironmentFreeze)
+        {
+            Debug.Log("Hi");
+            if (freezeEveryObject != null)
+            {
+                canInitiateEnvironmentFreeze = false;
+                StartCoroutine(freezeEveryObject(freezeEnvironmentTime));
+                StartCoroutine(ActivateEnvironmentCooldown());
+            }
+        }
+    }
+
+    /// <summary>
+    /// Denies the Player from freezing an object throughout the freeze single cooldown.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator ActivateSingleCooldown()
+    {
+        yield return waitForSingleCooldown;
+        canInitiateSingleFreeze = true;
+    }
+
+    /// <summary>
+    /// Denies the Player from freezing the environment throughout the freeze environment cooldown.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator ActivateEnvironmentCooldown()
+    {
+        yield return freezeEnvironmentCooldown;
+        canInitiateEnvironmentFreeze = true;
+    }
+}
--- Assets/Scripts/ObjectFreeze.cs
+++ Assets/Scripts/ObjectFreeze.cs
@@ -1,6 +1,5 @@
 /*
  * Freezes the attached gameobject for a specified amount of time; cooldown activates right when the object freezes. After the object is unfrozen, it can not be frozen whilst the cooldown is active.
- * The freeze button is [F].
  * 
  * Author: Cristion Dominguez
  * Date: 28 July 2021
@@ -12,72 +11,35 @@ using UnityEngine;
 
 public class ObjectFreeze : MonoBehaviour
 {
-    [SerializeField]
-    [Tooltip("The time the object shall be frozen for.")]
-    private float freezeTime = 5f;
-
-    [SerializeField]
-    [Tooltip("The duration the object can't be frozen after being frozen.")]
-    private float freezeCooldown = 5f;
-
-    // For suspending coroutines.
-    private WaitForSeconds waitForFreezeTime;
-    private WaitForSeconds waitForFreezeCooldown;
-
-    private bool canInitiateFreeze = true;  // Is the object unfrozen and is the cooldown inactive?
-
     private Rigidbody objectPhysics;  // for collecting and saving velocity and angular velocity
     private Vector3 unfrozenVelocity, unfrozenAngularVelocity;  // saved before freezing object
+
+    private FreezeInvocation playerFreeze;
 
     /// <summary>
     /// Gathers the rigidbody of the gameobject and assigns coroutine suspension times.
     /// </summary>
     private void Start()
     {
+        FreezeInvocation.freezeEveryObject += FreezeObject;
+
         objectPhysics = transform.GetComponent<Rigidbody>();
-
-        waitForFreezeTime = new WaitForSeconds(freezeTime);
-        waitForFreezeCooldown = new WaitForSeconds(freezeCooldown);
-    }
-
-    /// <summary>
-    /// Freezes the gameobject if the Player presses F, the object is unfrozen, and the cooldown is inactive.
-    /// </summary>
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.F) && canInitiateFreeze)
-        {
-            StartCoroutine(FreezeObject());
-        }
     }
 
     /// <summary>
     /// Freezes the gameobject and activates the cooldown, saving the object's velocity and angular velocity before doing so. After the freeze time is up, the velocity and angular
     /// velocity is returned to the object.
     /// </summary>
-    private IEnumerator FreezeObject()
+    public IEnumerator FreezeObject(float freezeTime)
     {
-        canInitiateFreeze = false;
-        StartCoroutine(ActivateCooldown());
-
         unfrozenVelocity = objectPhysics.velocity;
         unfrozenAngularVelocity = objectPhysics.angularVelocity;
         objectPhysics.isKinematic = true;
 
-        yield return waitForFreezeTime;
+        yield return new WaitForSeconds(freezeTime);
         
         objectPhysics.velocity = unfrozenVelocity;
         objectPhysics.angularVelocity = unfrozenAngularVelocity;
         objectPhysics.isKinematic = false;
-    }
-
-    /// <summary>
-    /// Denies the object from being frozen throughout the freeze cooldown.
-    /// </summary>
-    /// <returns></returns>
-    private IEnumerator ActivateCooldown()
-    {
-        yield return waitForFreezeCooldown;
-        canInitiateFreeze = true;
     }
 }
