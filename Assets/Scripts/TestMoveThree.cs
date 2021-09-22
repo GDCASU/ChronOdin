0cfdb329064725b4b711eef19d23ea73edc9d220
Phyrol <bpotzka@gmail.com>
2021-09-21 23:44:39 -0700

    Revert "merge conflicts"
    This reverts commit 30c8c0c8fd7d61fe0262411eb60a25ad0363f505, reversing
    changes made to 8abafa1804a1a7722a99575ee2d5e89f8c33e329.

--- Assets/Scripts/TestMoveThree.cs
+++ Assets/Scripts/TestMoveThree.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+public class TestMoveThree : MonoBehaviour
 {
     #region Variables
     public enum PlayerState
