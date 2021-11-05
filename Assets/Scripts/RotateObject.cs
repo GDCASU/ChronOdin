2fc5191d3fca558611aaf30677b3a73969be1b51
JsdCloud <jsdavi21@asu.edu>
2021-11-05 14:40:23 -0700

    Rotate object Basic
    rotates object - I think I'll add more options to it later

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+
+    private float timeScale; 
+
+    public float rotateSpeed;
+    public GameObject gameObject;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateTime();
+        MasterTime.singleton.updateTimeScaleEvent += UpdateTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateSpeed = rotateSpeed * timeScale * Time.fixedDeltaTime;
+
+        gameObject.transform.Rotate(0, rotateSpeed, 0, Space.Self);
+    }
+
+    private void UpdateTime() => timeScale = MasterTime.singleton.timeScale;
+}
