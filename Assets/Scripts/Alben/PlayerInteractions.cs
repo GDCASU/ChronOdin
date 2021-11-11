5390a59876d80d268482509a8d21c1411380a316
Christian Gonzalez <christiandash@hotmail.com>
2021-11-10 18:32:02 -0700

    Merged Scripts
    -merged Cristions pickUpObject and Albens playerInteractions script

--- Assets/Scripts/Alben/PlayerInteractions.cs
+++ Assets/Scripts/Alben/PlayerInteractions.cs
@@ -6,25 +6,29 @@ using UnityEngine;
 /// This script lets the player interact with objects in the scene such as doors or pickup objects.
 /// Author: Alben Trang
 /// </summary>
+[RequireComponent(typeof(ObjectPickup))]
 public class PlayerInteractions : MonoBehaviour
 {
-    [Tooltip("The player's first-person camera is used as a reference point for interactions")]
-    public Camera playerCamera;
-    [Tooltip("Set an empty game object in front and as a child of the player camera to hold objects")]
-    public Transform pickupDest;
-    [Tooltip("Set how far the player can interact with objects with the press of Fire1 (left mouse button)")]
-    public float range = 10.0f;
+    [HideInInspector] public static PlayerInteractions singleton;
 
-    private bool isHolding;
+    [Tooltip("The camera attached to Player")]
+    [SerializeField]
+    public Transform playerCamera;
 
-    /// <summary>
-    /// Start on frame on that the player isn't holding anything
-    /// </summary>
-    private void Start()
+    [Header("Properties")]
+    [SerializeField]
+    [Tooltip("The maximum distance the Player can pick up an object")]
+    private float maxInteractDistance = 3f;
+
+    [HideInInspector] public RaycastHit rayHit;
+
+    private void Awake()
     {
-        isHolding = false;
+        if (singleton == null)
+            singleton = this;
+        else
+            Destroy(gameObject);
     }
-
     /// <summary>
     /// Check every frame to see if the player hits the Fire1 button (left mouse button) to interact with objects
     /// </summary>
@@ -32,55 +36,13 @@ public class PlayerInteractions : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            if (!isHolding && Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out RaycastHit hit, range))
+            if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out rayHit, maxInteractDistance))
             {
-                // Interact with a door
-                if (hit.transform.gameObject.GetComponent<Door>())
-                {
-                    InteractDoor(hit);
-                }
-                // Interact with pickup object
-                else if (hit.transform.gameObject.GetComponent<PickupObject>())
-                {
-                    PickUpObject(hit);
-                } 
-            }
-            else if (isHolding)
-            {
-                DropObject();
+                if (rayHit.transform.tag.Equals("Liftable"))GetComponent<ObjectPickup>().PickupObject();
+                else if (rayHit.transform.tag.Equals("Interactable")) rayHit.transform.GetComponent<InteractiveObject>().Interact();
             }
         }
-    }
-
-    /// <summary>
-    /// Interacts with the door to open or close it
-    /// </summary>
-    /// <param name="hit">The object the camera is pointing at</param>
-    private void InteractDoor(RaycastHit hit)
-    {
-        hit.transform.gameObject.GetComponent<Door>().InteractWithDoor();
-    }
-
-    /// <summary>
-    /// Picks up a pickup object that has the PickupObject script attatched to it
-    /// </summary>
-    /// <param name="hit">The object the camera is pointing at</param>
-    private void PickUpObject(RaycastHit hit)
-    {
-        hit.transform.gameObject.GetComponent<PickupObject>().Pickup(pickupDest);
-        isHolding = true;
-    }
-
-    /// <summary>
-    /// Drop the object the player is holding
-    /// </summary>
-    private void DropObject()
-    {
-        Collider[] colliders = Physics.OverlapBox(pickupDest.GetChild(0).transform.position, pickupDest.GetChild(0).transform.localScale / 2);
-        if (colliders.Length == 0) // Makes sure that the pickup object isn't colliding with anything else before being dropped
-        { 
-            pickupDest.GetChild(0).transform.gameObject.GetComponent<PickupObject>().Drop(pickupDest);
-            isHolding = false;
-        }
+        else if(Input.GetButtonUp("Fire1"))
+            if (rayHit.transform.tag.Equals("Liftable")) GetComponent<ObjectPickup>().PickupObject();
     }
 }
--- Assets/Scripts/ObjectPickup.cs
+++ Assets/Scripts/ObjectPickup.cs
@@ -7,22 +7,13 @@
  */
 
 using UnityEngine;
-
 public class ObjectPickup : MonoBehaviour
 {
     [Header("Transforms")]
-    [Tooltip("The camera attached to Player")]
-    [SerializeField]
-    Transform playerCamera;
 
     [Tooltip("An empty Transform that the held object shall follow")]
     [SerializeField]
     Transform target;
-
-    [Header("Properties")]
-    [SerializeField]
-    [Tooltip("The maximum distance the Player can pick up an object")]
-    private float maxPickupDistance = 3f;
 
     [Header("Properties")]
     [SerializeField]
@@ -41,39 +32,14 @@ public class ObjectPickup : MonoBehaviour
 
     private Transform heldObject = null;  // the transform of the picked up object
     private Rigidbody objectRb = null;  // the rigidbody of the picked up object
-    private RaycastHit rayHit;  // info about ray collisions
     
     // The previous interpolation and detection mode of the picked up object.
     private RigidbodyInterpolation previousInterpolation = RigidbodyInterpolation.None;
     private CollisionDetectionMode previousDetectionMode = CollisionDetectionMode.Discrete;
 
-    // Values for casting a ray to detect collisions.
-    Vector3 startRayPosition;  // starting position of ray
-    Vector3 rayDirection;  // direction of ray
-
     // Values for calculating held object velocity.
     Vector3 directionToTarget;  // direction from held object to target
     float distanceToTarget;  // distance between held object and target
-
-    /// <summary>
-    /// Attempts to pick up an object when Player presses down [Tab]. 
-    /// Releases object when the Player ceases pressing down [Tab].
-    /// </summary>
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            Pickup();
-        }
-
-        if (heldObject != null)
-        {
-            if (Input.GetKeyUp(KeyCode.E))
-            {
-                Release();
-            }
-        }
-    }
 
     /// <summary>
     /// Grants the held object velocity based on the direction to target, distance from target, and speed.
@@ -86,76 +52,52 @@ public class ObjectPickup : MonoBehaviour
             distanceToTarget = Vector3.Distance(target.position, objectRb.position);
             objectRb.velocity = directionToTarget * distanceToTarget * speed * Time.fixedDeltaTime;
             Vector3 dirToPlayer = (transform.position - heldObject.position);
-            Physics.Raycast(heldObject.position, dirToPlayer.normalized, out rayHit, dirToPlayer.magnitude);
+            Physics.Raycast(heldObject.position, dirToPlayer.normalized, out RaycastHit rayHit, dirToPlayer.magnitude);
             Debug.DrawLine(heldObject.position, heldObject.position+dirToPlayer,Color.red);
-            if (rayHit.collider != null && !rayHit.transform.CompareTag("Player")) Release();
+            if (rayHit.collider != null && !rayHit.transform.CompareTag("Player")) ReleaseObject();
             //print(Vector3.Angle(-transform.up, directionToTarget));
             //Debug.Log(objectPhysics.velocity.magnitude);
         }
     }
-
     /// <summary>
-    /// Casts a ray from playerCamera in the forward direction.
-    /// If the ray hits the Player, another ray is casted a distance away (rayPositionOffset) from the hit position in the previous ray's direction.
-    ///   This can be done for a finite amount of times (maxRayCasts).
-    /// If the ray hits an object with the "Liftable" tag, the transform and rigidbody of the object is captured. Then the object's gravity is deactivated,
+    ///The transform and rigidbody of the object is captured. Then the object's gravity is deactivated,
     ///   and its interpolation and detection modes are altered. Then the target position is centered at the held object's current position.
     /// If the ray hits no collider, nothing occurs.
     /// </summary>
-    private void Pickup()
+    public void PickupObject()
     {
-        // Set the ray's starting position and direction.
-        startRayPosition = playerCamera.position;
-        rayDirection = playerCamera.TransformDirection(Vector3.forward);
-
-        // Cast the ray until the ray does not hit the Player or maxRayCasts has been reached.
-        if (Physics.Raycast(startRayPosition, rayDirection, out rayHit, maxPickupDistance))
+        // If the ray hits a Liftable, prepare the object for holding and stop casting rays.
+        if (PlayerInteractions.singleton.rayHit.transform.tag.Equals("Liftable"))
         {
-            // If the ray hits a Liftable, prepare the object for holding and stop casting rays.
-            if (rayHit.transform.tag.Equals("Liftable"))
-            {
-                // Capture the object's transform and rigidbod
-                heldObject = rayHit.transform;
-                //print(Vector3.Angle(-transform.up, (heldObject.position-Camera.main.transform.position).normalized));
-                objectRb = heldObject.GetComponent<Rigidbody>();
-                originalLayer = heldObject.gameObject.layer;
-                heldObject.gameObject.layer = newLayer;
+            // Capture the object's transform and rigidbod
+            heldObject = PlayerInteractions.singleton.rayHit.transform;
+            //print(Vector3.Angle(-transform.up, (heldObject.position-Camera.main.transform.position).normalized));
+            objectRb = heldObject.GetComponent<Rigidbody>();
+            originalLayer = heldObject.gameObject.layer;
+            heldObject.gameObject.layer = newLayer;
 
-                // Alter the object's physics properties.
-                objectRb.useGravity = false;
-                previousInterpolation = objectRb.interpolation;
-                previousDetectionMode = objectRb.collisionDetectionMode;
-                objectRb.interpolation = RigidbodyInterpolation.Interpolate;
-                objectRb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
-                objectRb.freezeRotation = true;
+            // Alter the object's physics properties.
+            objectRb.useGravity = false;
+            previousInterpolation = objectRb.interpolation;
+            previousDetectionMode = objectRb.collisionDetectionMode;
+            objectRb.interpolation = RigidbodyInterpolation.Interpolate;
+            objectRb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+            objectRb.freezeRotation = true;
 
-                // Center target at held object.
-                target.position = playerCamera.transform.position + playerCamera.transform.forward * distanceFromPlayer;
-                return;
-            }
-            // If the ray hits nothing, stop casting rays.
-            // FOR TESTING PURPOSES, comment this "else" block out.
-            else
-            {
-                return;
-            }
+            // Center target at held object.
+            target.position = PlayerInteractions.singleton.playerCamera.transform.position + PlayerInteractions.singleton.playerCamera.transform.forward * distanceFromPlayer;
+            return;
         }
-        
-
-        // FOR TESTING PURPOSES, remove the comments for the block below.
-        /*
-         if (rayHit.transform != null)
-            Debug.Log(rayHit.transform.name);
-         else
-            Debug.Log("N/A");
-        */
+        // If the ray hits nothing, stop casting rays.
+        // FOR TESTING PURPOSES, comment this "else" block out.
+        else if (PlayerInteractions.singleton.rayHit.transform.tag.Equals("Interactable")) PlayerInteractions.singleton.rayHit.transform.GetComponent<InteractiveObject>().Interact();
     }
 
     /// <summary>
     /// Releases the held object from Player's grasp, which restores the object's properties before being
     /// picked up.
     /// </summary>
-    private void Release()
+    public void ReleaseObject()
     {
         // Restore object properties before pickup.
         objectRb.useGravity = true;
