65598be6d475a457dd6a7a1d6e8ec134f569cdba
MaelstromDef <achuggi1@asu.edu>
2022-04-08 13:32:29 -0700

    Add Note and Fix MPs
    Added the "Interactive Note" to the level, and fixed the coding errors that caused the IndexOutOfRangeException's in the MovingPlatform.cs file.

--- Assets/Scripts/LevelFunctionality/MovingPlatformPath.cs
+++ Assets/Scripts/LevelFunctionality/MovingPlatformPath.cs
@@ -29,7 +29,7 @@ public class MovingPlatformPath : SimpleTimeManipulation
             float step = 0;
             while (step < 1)
             {
-                step += speed * speedMultiplier * timeScale * Time.fixedDeltaTime * 1f / Vector3.Distance(points[i], points[i - 1]);
+                step += speed * speedMultiplier * timeScale * Time.fixedDeltaTime * 1f / Vector3.Distance(points[i], points[i + 1]);
                 platform.transform.position = transform.position + Vector3.Lerp(points[i], points[i + 1], step);
                 yield return new WaitForFixedUpdate();
             }
@@ -45,7 +45,7 @@ public class MovingPlatformPath : SimpleTimeManipulation
             float step = 0;
             while (step < 1)
             {
-                step += speed * speedMultiplier * timeScale * Time.fixedDeltaTime * 1f / Vector3.Distance(points[i], points[i + 1]);
+                step += speed * speedMultiplier * timeScale * Time.fixedDeltaTime * 1f / Vector3.Distance(points[i], points[i - 1]);
                 platform.transform.position = transform.position + Vector3.Lerp(points[i], points[i - 1], step);
                 yield return new WaitForFixedUpdate();
 
