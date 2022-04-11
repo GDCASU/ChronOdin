d1c8a29f45d2e18d6108d5fb9ddc97a4cba5a962
Christian Gonzalez <christiandash@hotmail.com>
2022-04-11 16:00:10 -0700

    Script Fix

--- Assets/Scripts/LevelFunctionality/MovingPlatformPath.cs
+++ Assets/Scripts/LevelFunctionality/MovingPlatformPath.cs
@@ -29,7 +29,7 @@ public class MovingPlatformPath : SimpleTimeManipulation
             float step = 0;
             while (step < 1)
             {
-                step += speed * speedMultiplier * timeScale * Time.fixedDeltaTime * 1f / Vector3.Distance(points[i], points[i - 1]);
+                step += speed * speedMultiplier * timeScale * Time.fixedDeltaTime * 1f / Vector3.Distance(points[i], points[i + 1]);
                 platform.transform.position = transform.position + Vector3.Lerp(points[i], points[i + 1], step);
                 yield return new WaitForFixedUpdate();
             }
@@ -40,12 +40,12 @@ public class MovingPlatformPath : SimpleTimeManipulation
     IEnumerator MovePlatformBackWards()
     {
         platform.transform.position = transform.position + points[0];
-        for (int i = points.Length - 1; i > 1 - 1; i--)
+        for (int i = points.Length - 1; i > 0; i--)
         {
             float step = 0;
             while (step < 1)
             {
-                step += speed * speedMultiplier * timeScale * Time.fixedDeltaTime * 1f / Vector3.Distance(points[i], points[i + 1]);
+                step += speed * speedMultiplier * timeScale * Time.fixedDeltaTime * 1f / Vector3.Distance(points[i], points[i - 1]);
                 platform.transform.position = transform.position + Vector3.Lerp(points[i], points[i - 1], step);
                 yield return new WaitForFixedUpdate();
 
